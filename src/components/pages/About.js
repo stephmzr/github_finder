6b1abeba5ce665cc97f92c19aa983e8ac2f3bcb8
stephmzr <stephan.mazur5@gmail.com>
2021-03-18 01:19:32 +0100

    install react router & component about

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+    <div>
+      <h1>About this app</h1>
+      <p>App to search Github users</p>
+    </div>
+    </Fragment>
+  )
+}
+
+export default About;
