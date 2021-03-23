fffc2601b453d5d03d00d6a98aac55a1f3b99338
stephmzr <stephan.mazur5@gmail.com>
2021-03-23 20:42:13 +0100

    creation reducers et github context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -21,6 +21,16 @@ const GithubState = props => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   //Search users
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    
+    dispatch({ type: SEARCH_USERS, payload: res.data.items})
+  }
 
   //Get User
 
@@ -29,6 +39,7 @@ const GithubState = props => {
   //Clear users
 
   //Set loading
+  const setLoading = () => dispatch({ type: SET_LOADING});
 
   return (
   <GithubContext.Provider
@@ -36,7 +47,8 @@ const GithubState = props => {
       users: state.users,
       user: state.user,
       repos: state.repos,
-      loading: state.loading
+      loading: state.loading,
+      searchUsers
     }}
     >
       {props.children}
