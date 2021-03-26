9dadbd0be1657152d1691355d74ed79f4d9e3e55
stephmzr <stephan.mazur5@gmail.com>
2021-03-26 17:55:27 +0100

    home et notfound component

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>Not found</h1>
+      <p className='lead'>This page does not exist !</p>
+    </div>
+  )
+}
